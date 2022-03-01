e846d89dafb2712cf0317a42778b6417818e6f46
H. Avni Cubukcu <55025103+avni25@users.noreply.github.com>
2022-03-01 06:30:40 +0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    MONGO_URI: "mongodb://admin:oO6XrKyFBT97ma4y@cluster0-shard-00-00.ar8pp.mongodb.net:27017,cluster0-shard-00-01.ar8pp.mongodb.net:27017,cluster0-shard-00-02.ar8pp.mongodb.net:27017/users?ssl=true&replicaSet=atlas-itps3e-shard-0&authSource=admin&retryWrites=true&w=majority"
-}

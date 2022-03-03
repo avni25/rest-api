ac0f8b49810ed304e0f26f7fbf4389deb9fdea67
H. Avni Cubukcu <huseyinac@gmail.com>
2022-03-03 07:04:07 +0300

    some deleted

--- main.js
+++
@@ -1,8 +0,0 @@
-console.log("qwe");
-const mytext = document.getElementById("mytext");
-const button = document.getElementById("mybtn");
-        
-button.addEventListener("click", ()=>{
-    console.log(mytext.value);
-    window.location = `http://localhost:3000/aircrafts?type=${mytext.value}`
-});

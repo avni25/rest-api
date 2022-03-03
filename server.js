769d67e0993fe9c96618d8552c4ad808715af6bf
H. Avni Cubukcu <huseyinac@gmail.com>
2022-03-03 09:03:27 +0300

    ..

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const aircraftRoutes = require("./routes/aircrafts-route");
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.DATABASE_URL || '0.0.0.0';
+
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
 
 // connect to MongoDB
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+mongoose.connect(HOST, {useNewUrlParser: true})
     .then(()=> console.log("MongoDB connected!!"))
     .catch((err)=> console.log(err));
 

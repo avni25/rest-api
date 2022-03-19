b7b6f7589f7081a77fb54fbe2fbd801eba735f83
H. Avni Cubukcu <huseyinac@gmail.com>
2022-03-19 07:13:08 +0300

    ..

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const bodyParser = require("body-parser");
 const aircraftRoutes = require("./routes/aircrafts-route");
 const path = require('path');
@@ -10,6 +11,7 @@ if (port == null || port == "") {
   port = 8000;
 }
 const HOST = process.env.DATABASE_URL || '0.0.0.0';
+const client = new MongoClient(HOST, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const app = express();
 app.use(express.json());
